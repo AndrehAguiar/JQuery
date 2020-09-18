14559a232d1b53afce0c408b26f4e206c6396f41
AndrehAguiar <andreaguiar.g@gmail.com>
2020-09-17 22:28:22 -0300

    Add slide auto rotate

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,62 @@
-window.addEventListener('load', function () {
+$(function () {
 
-    let next = function (arg) {
-
-        for (id of lstSlide) {
-
-            id == arg ? $(arg)
-                .fadeIn("slow", "linear")
-                : $(id)
-                    .hide();
-        }
-    }
-
+    let position = 0;
     const lstSlide = ['#1', '#2', '#3', '#4', '#5'];
 
-    const l1 = document.getElementById('l1');
-    const l2 = document.getElementById('l2');
-    const l3 = document.getElementById('l3');
-    const l4 = document.getElementById('l4');
-    const l5 = document.getElementById('l5');
+    let change = function (arg) {
 
-    l1.addEventListener('click', function () {
-        next(lstSlide[0])
+        for (id of lstSlide) {
+            id == arg ? $(arg).fadeIn("slow", "linear") : $(id).hide();
+        }
+    };
+
+    let carroussel = function () {
+        if (position >= lstSlide.length) {
+            position = 0;
+        }
+        change(lstSlide[position]);
+        position++;
+    };
+
+    let auto = function () {
+        setInterval(carroussel, 5000);
+    };
+
+    $('#l1').click(function () {
+        change(lstSlide[0]);
+        position = 0;
     });
 
-    l2.addEventListener('click', function () {
-        next(lstSlide[1])
+    $('#l2').click(function () {
+        change(lstSlide[1]);
+        position = 1;
     });
 
-    l3.addEventListener('click', function () {
-        next(lstSlide[2])
+    $('#l3').click(function () {
+        change(lstSlide[2]);
+        position = 2;
     });
 
-    l4.addEventListener('click', function () {
-        next(lstSlide[3])
+    $('#l4').click(function () {
+        change(lstSlide[3]);
+        position = 3;
     });
 
-    l5.addEventListener('click', function () {
-        next(lstSlide[4])
+    $('#l5').click(function () {
+        change(lstSlide[4]);
+        position = 4;
     });
 
+    $('#goRight').click(function () {
+        position++
+        if (position > 4) position = 0;
+        change(lstSlide[position]);
+    });
+
+    $('#goLeft').click(function () {
+        position--
+        if (position < 0) position = 4;
+        change(lstSlide[position]);
+    });
+    window.onload = auto;
 });
